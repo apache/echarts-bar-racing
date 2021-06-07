115f33214c127ea9c11019751d5ef7a721424f43
Ovilia <zwl.sjtu@gmail.com>
2021-06-07 17:12:18 +0800

    chart and table

--- src/components/BTable.vue
+++ src/components/BTable.vue
@@ -1,34 +1,58 @@
 <template>
     <div class="w-full h-full">
-        <div class="grid grid-cols-3 h-full">
-            <div id="el-table" class="col-span-2 h-full" style="border: 1px solid red;"></div>
-            <div id="el-config" class="p-3 align-middle">
-                <div>
-                    <el-button type="button" onclick="run()" size="medium">运行</el-button>
-                    <el-button type="button" size="medium">导出</el-button>
+        <div class="grid grid-cols-12 h-full text-sm">
+            <el-card class="box-card col-span-3">
+                <h1 slot="header" class="clearfix text-xl">
+                    Apache ECharts Bar-Race 生成器
+                </h1>
+                <div id="el-config" class="align-middle">
+                    <!-- <div class="my-3">
+                        <el-button onclick="run()" size="medium">运行</el-button>
+                        <el-button size="medium">导出</el-button>
+                    </div> -->
+                    <el-form ref="form">
+                        <div class="grid grid-cols-3 form-row">
+                            <label class="col-span-1">标题</label>
+                            <el-input id="input-title" value="汽车产量动态排名" size="medium" class="col-span-2"></el-input>
+                        </div>
+                        <div class="grid grid-cols-3 form-row">
+                            <label class="col-span-1">显示排名上限</label>
+                            <el-input id="input-max" type="number" value="10" size="medium" class="col-span-2"></el-input>
+                        </div>
+                    </el-form>
                 </div>
-                <el-form ref="form">
-                    <div class="grid grid-cols-2 form-row">
-                        <label>标题</label>
-                        <el-input id="input-title" value="汽车产量动态排名" size="medium"></el-input>
-                    </div>
-                    <div class="grid grid-cols-2 form-row">
-                        <label>显示排名上限</label>
-                        <el-input id="input-max" type="number" value="10" size="medium"></el-input>
-                    </div>
-                </el-form>
-            </div>
+            </el-card>
+            <el-card class="box-card col-span-4 relative" body-style="height: 100%">
+                <div slot="header" class="clearfix text-base">
+                    数据
+                </div>
+                <div id="table-panel" class="overflow-auto absolute bottom-4 top-14 left-5 right-5 border">
+                </div>
+            </el-card>
+            <el-card class="box-card col-span-5 relative" body-style="height: 100%">
+                <div slot="header" class="clearfix text-base">
+                    预览
+                    <a href="#">
+                        <i class="el-icon-refresh"></i>
+                    </a>
+                </div>
+                <div id="bar-race-preview" class="absolute bottom-4 top-14 left-5 right-5 border">
+                </div>
+            </el-card>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent} from 'vue';
+import btable from './btable';
+
 export default defineComponent({
     name: 'BTable',
     props: {
     },
-    setup: () => {
+    mounted: () => {
+        btable.initTable();
     }
 })
 </script>
@@ -41,6 +65,10 @@ export default defineComponent({
         label {
             @apply py-1;
         }
+    }
+
+    .box-card {
+        @apply m-1;
     }
 }
 </style>
